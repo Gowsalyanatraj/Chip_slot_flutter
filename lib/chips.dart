309c5bed6780a2c470da12c851241afdf83ec0e8
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-12-06 16:46:19 +0530

    creating chip slots in custom formfield

---
+++ lib/chips.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class Chip_Slots extends StatelessWidget {
+  const Chip_Slots({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
