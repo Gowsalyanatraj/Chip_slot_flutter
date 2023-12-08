f4eee5fe015b1dcb231402a811faf8e026970d79
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-12-08 16:11:09 +0530

    creating chips main page

--- lib/chips.dart
+++ lib/chips.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
-class Chip_Slots extends StatelessWidget {
+
+class Chip_Slots extends StatefulWidget {
   const Chip_Slots({super.key});
 
   @override
+  State<Chip_Slots> createState() => _Chip_SlotsState();
+}
+
+class _Chip_SlotsState extends State<Chip_Slots> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> skills = [];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("Add your skills"),
+        ),
     );
   }
 }
