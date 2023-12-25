a48bc488fbd5732325f64c69de23d4260ab53796
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-12-25 17:58:57 +0530

    chore

--- lib/Chip_field.dart
+++ lib/Chip_field.dart
@@ -10,7 +10,7 @@ class Chip_field extends StatelessWidget {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
-          labelText: "Add your skill",
+          labelText: "Pick your Skill",
           suffixIcon: IconButton(
               onPressed: () {
                 onSubmitted(controller.text);
