fb2a96c56128c97b5bb89918c23500e58f2bfc3b
gowsalyanatraj <pinkygowsi717@gmail.com>
2023-12-30 16:02:19 +0530

    chore

--- lib/chips.dart
+++ lib/chips.dart
@@ -1,60 +1,60 @@
-// import 'package:chips/Chip_field.dart';
-// import 'package:flutter/material.dart';
+import 'package:chips/Chip_field.dart';
+import 'package:flutter/material.dart';
 
-// class Chip_Slots extends StatefulWidget {
-//   const Chip_Slots({super.key});
+class Chip_Slots extends StatefulWidget {
+  const Chip_Slots({super.key});
 
-//   @override
-//   State<Chip_Slots> createState() => _Chip_SlotsState();
-// }
+  @override
+  State<Chip_Slots> createState() => _Chip_SlotsState();
+}
 
-// class _Chip_SlotsState extends State<Chip_Slots> {
-//   final TextEditingController _controller = TextEditingController();
-//   List<String> skills = [];
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text("Add your skills"),
-//       ),
-//       body: Column(
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.all(8.0),
-//             child: Chip_field(
-//                 controller: _controller,
-//                 onSubmitted: (value) {
-//                   if (value.isNotEmpty) {
-//                     addSkill(value);
-//                     _controller.clear();
-//                   }
-//                 }),
-//           ),
-//           Wrap(
-//             spacing: 8.0,
-//             children: skills.map((skill) {
-//               return Chip(
-//                 label: Text(skill),
-//                 onDeleted: () {
-//                   removeSkill(skill);
-//                 },
-//               );
-//             }).toList(),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
+class _Chip_SlotsState extends State<Chip_Slots> {
+  final TextEditingController _controller = TextEditingController();
+  List<String> skills = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add your skills"),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Chip_field(
+                controller: _controller,
+                onSubmitted: (value) {
+                  if (value.isNotEmpty) {
+                    addSkill(value);
+                    _controller.clear();
+                  }
+                }),
+          ),
+          Wrap(
+            spacing: 8.0,
+            children: skills.map((skill) {
+              return Chip(
+                label: Text(skill),
+                onDeleted: () {
+                  removeSkill(skill);
+                },
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
 
-//   void addSkill(String Skill) {
-//     setState(() {
-//       skills.add(skills as String);
-//     });
-//   }
+  void addSkill(String Skill) {
+    setState(() {
+      skills.add(skills as String);
+    });
+  }
 
-//   void removeSkill(String skill) {
-//     setState(() {
-//       skills.remove(skill);
-//     });
-//   }
-// }
+  void removeSkill(String skill) {
+    setState(() {
+      skills.remove(skill);
+    });
+  }
+}
